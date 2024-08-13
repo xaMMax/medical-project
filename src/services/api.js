b1c8f45212b20308c0d811ca89fd7d72a2dff95c
xammax <petrolijuice@gmail.com>
2024-08-13 12:04:24 +0200

    update proj

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import router from 'src/router/index';  // Імпорт маршрутизатора
 
-const API_URL = 'https://xammax.pythonanywhere.com/';  // URL до вашого бекенду на Django з префіксом /api/
+const API_URL = 'https://xammax.pythonanywhere.com/api/';
 
 const apiClient = axios.create({
   baseURL: API_URL,
@@ -9,46 +10,43 @@ const apiClient = axios.create({
   }
 });
 
-// Middleware для додавання токену авторизації
 apiClient.interceptors.request.use(config => {
-  const token = localStorage.getItem('accessToken');  // Використовуємо access токен
+  const token = localStorage.getItem('accessToken');
   if (token) {
     config.headers['Authorization'] = `Bearer ${token}`;
   }
   return config;
 });
 
-// Middleware для обробки відповідей, включаючи автоматичне оновлення access токену
 apiClient.interceptors.response.use(
-    response => response,
-    async error => {
-      const originalRequest = error.config;
-  
-      if (error.response.status === 401 && !originalRequest._retry) {
-        originalRequest._retry = true;
-        const refreshToken = localStorage.getItem('refreshToken');
-  
-        if (refreshToken) {
-          try {
-            const { data } = await axios.post(`${API_URL}token/refresh/`, { refresh: refreshToken });
-            localStorage.setItem('accessToken', data.access);
-            originalRequest.headers['Authorization'] = `Bearer ${data.access}`;
-            return apiClient(originalRequest);
-          } catch (err) {
-            console.error('Refresh token failed', err);
-            localStorage.removeItem('accessToken');
-            localStorage.removeItem('refreshToken');
-            this.$router.push('/login');
-          }
-        } else {
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem('refreshToken');
+
+      if (refreshToken) {
+        try {
+          const { data } = await axios.post(`${API_URL}token/refresh/`, { refresh: refreshToken });
+          localStorage.setItem('accessToken', data.access);
+          originalRequest.headers['Authorization'] = `Bearer ${data.access}`;
+          return apiClient(originalRequest);
+        } catch (err) {
+          console.error('Refresh token failed', err);
           localStorage.removeItem('accessToken');
           localStorage.removeItem('refreshToken');
-          this.$router.push('/login');
+          await router.push('/login');  // Використання маршрутизатора
         }
+      } else {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        await router.push('/login');  // Використання маршрутизатора
       }
-      return Promise.reject(error);
     }
-  );
-  
+    return Promise.reject(error);
+  }
+);
 
 export default apiClient;
